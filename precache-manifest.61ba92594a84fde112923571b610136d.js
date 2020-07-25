d4dc5c717cfb58e4ebfd3f873e98ff21a19c40af
mikk7440 <miols15@student.sdu.dk>
2020-07-25 23:56:37 +0200

    Updates

---
+++ precache-manifest.61ba92594a84fde112923571b610136d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c835bffb425771c1c6b7525f09443558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874b47e21b1d5c43200c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "19ff84619b9e7115e341",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "874b47e21b1d5c43200c",
+    "url": "/static/js/2.6f50c311.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.6f50c311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ff84619b9e7115e341",
+    "url": "/static/js/main.6bc24de1.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "9f989e84acc7d404c6a9fb1af981f9ad",
+    "url": "/static/media/profile.9f989e84.png"
+  },
+  {
+    "revision": "9a7fd799e55033f7440402df8f20bbb2",
+    "url": "/static/media/resume.9a7fd799.pdf"
+  }
+]);
